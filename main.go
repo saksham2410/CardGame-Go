79183debfdbc02d7201b34811abdb092baabdbf3
Saksham <gupta.10@iitj.ac.in>
2019-09-29 04:37:33 +0530

    playing around with types

--- main.go
+++ main.go
@@ -1,19 +1,18 @@
 package main
 
-import "fmt"
-
 func main() {
 	// var card string = "Ace of cards"
 
 	// cards := "Ace of cards"
-	cards := []string{"Meow", newCard()}
+	cards := deck{"Meow", newCard()}
 	cards = append(cards, "saksham")
 	//only define colon when initially you have to define a value
 	// card = "Meow"
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
+	// for i, card := range cards {
+	// 	fmt.Println(i, card)
+	// }
 
+	cards.print()
 	// fmt.Println(card)
 }
 func newCard() string {
