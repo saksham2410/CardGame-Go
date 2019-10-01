d75efe4793ce5b3f558dba1b3fe2d830bd08f3e8
Saksham <gupta.10@iitj.ac.in>
2019-10-02 03:14:12 +0530

    added functions

--- main.go
+++ main.go
@@ -1,20 +1,7 @@
 package main
 
 func main() {
-	// var card string = "Ace of cards"
-
-	// cards := "Ace of cards"
-	cards := deck{"Meow", newCard()}
-	cards = append(cards, "saksham")
-	//only define colon when initially you have to define a value
-	// card = "Meow"
-	// for i, card := range cards {
-	// 	fmt.Println(i, card)
-	// }
-
+	cards := newDeck()
+	cards.shuffle()
 	cards.print()
-	// fmt.Println(card)
-}
-func newCard() string {
-	return "Meow modified"
 }
